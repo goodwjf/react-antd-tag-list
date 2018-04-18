bc1da8d1a60c391d2720527b09b594dab86eaf98
goodwjf <wangjianfenga1@163.com>
2018-04-18 16:47:55 +0800

    删除沉余代码

--- src/index.jsx
+++ src/index.jsx
@@ -17,24 +17,14 @@ let add = (tag, tags) => {
 }
 
 render(
-  <Frame childData={{
-    'name': '安全后台管理系统',
-    'key': '4'
-  }}>
-    <div className={css.header}>
-     安全后台管理系统
-    </div>
-    <div className={css.content}>
-      <TagList
-        width={200}
-        height={500}
-        onAdd={add}
-        onRemoved={removed}
-        onSelected={selected}
-        data={['任务1', '任务2', '任务3']}
-        plusName='添加任务'
-      />
-    </div>
-  </Frame>,
+  <TagList
+    width={200}
+    height={500}
+    onAdd={add}
+    onRemoved={removed}
+    onSelected={selected}
+    data={['任务1', '任务2', '任务3']}
+    plusName='添加任务'
+  />,
   document.getElementById('root')
 )
