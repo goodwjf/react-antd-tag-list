7dcaf93bf7ab7fe7880342255e4a22e4c0f73acc
goodwjf <wangjianfenga1@163.com>
2018-04-20 15:54:26 +0800

    支持tag排序

--- src/components/TagList/index.js
+++ src/components/TagList/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Tag, Input, Tooltip, Icon, Modal, Pagination } from 'antd';
 import css from './index.scss'
 const confirm = Modal.confirm;
-
+let dragElement = null
 let MyTag = (props) => {
   let type = props.icon.type || 'close'
   let icon = {
@@ -11,12 +11,61 @@ let MyTag = (props) => {
       node: (<Icon type="plus" className={css['icon-action']} />)
     },
     'close': {
-      className: css['my-tag'],
+      className: [css['my-tag']].join(' '),
       node: (<Icon type="close" className={css['icon-action']} onClick={props.icon.event} />)
     }
   }
+  let dragStart = (e) => {
+    e.target.style.opacity = '0.5'
+    dragElement = e.target
+    e.dataTransfer.effectAllowed = "move"
+  }
+  let dragEnter = (e) => {
+    e.target.classList.add(css.over)
+  }
+  let dragOver = (e) => {
+    e.preventDefault()
+    e.dataTransfer.dropEffect = 'move'
+    return false
+  }
+  let dragLeave = (e) => {
+    e.target.classList.remove(css.over)
+  }
+  let drop = (e) => {
+    e.stopPropagation()
+    if (dragElement != e.target) {
+      let opt = {}
+      opt[e.target.dataset.index] = dragElement.dataset.val
+      opt[dragElement.dataset.index] = e.target.dataset.val
+      props.dataUpdate(opt)
+    }
+    return false;
+  }
+  let dragEnd = (e) => {
+    let dorgTags = document.querySelectorAll('div[type="close"]')
+    dorgTags.forEach((tag) => {
+      tag.classList.remove(css.over);
+      tag.style.opacity = '1';
+    })
+  }
+
+  
+  let _config ={
+      onDragStart: dragStart,
+      onDragEnter: dragEnter,
+      onDragOver: dragOver,
+      onDragLeave: dragLeave,
+      onDrop: drop,
+      onDragEnd: dragEnd,
+      draggable: "true"
+  }
+  let config = type === 'close' ? _config : {}
   return (
-    <div 
+    <div
+      {...config}
+      type={type}
+      data-val={props.val}
+      data-index={props.index}
       className={icon[type].className}
       onClick={props.onClick}
     >
@@ -28,18 +77,19 @@ let MyTag = (props) => {
 
 export default class Tasks extends Component {
   state = {
-    data: this.props.data,
+    data: [],
     tags: [],
     inputVisible: false,
     inputValue: '',
   };
 
   componentDidMount() {
-    this.setState({tags: this.state.data})
+    this.setState({ data: this.props.data })
+    this.setState({ tags: this.props.data })
   }
 
   handleClose = (e) => {
-    let removedTag = e.target.parentNode.textContent
+    let removedTag = e.target.textContent
     confirm({
       title: '提示！',
       content: '确认要删除吗？',
@@ -51,7 +101,7 @@ export default class Tasks extends Component {
         this.setState({ tags, data }, () => {
           this.selectedTagNode && this.selectedTagNode.classList.add(css['tag-selected'])
         });
-        this.props.onRemoved(removedTag, tags)
+        this.props.onRemoved(removedTag, data)
       }
     });
     e.stopPropagation();
@@ -81,10 +131,10 @@ export default class Tasks extends Component {
     let data = state.data;
     if (inputValue && data.indexOf(inputValue) === -1) {
       data = [...data, inputValue];
+      this.props.onAdd(inputValue, data)
     }
     if (inputValue && tags.indexOf(inputValue) === -1) {
       tags = [...tags, inputValue];
-      this.props.onAdd(inputValue, tags)
     }
     this.setState({
       data,
@@ -104,22 +154,30 @@ export default class Tasks extends Component {
     }
     this.setState({tags})
   }
+
+  dataUpdate = (opt) => {
+    let data = this.state.data
+    for(let key in opt) {
+      data[key] = opt[key]
+    }
+    this.setState({data})
+  }
   searchInputRef = input => this.searchInput = input
   saveInputRef = input => this.input = input
 
   render() {
     const { tags, inputVisible, inputValue } = this.state
-    const { width, height, pageSize, plusName } = this.props
+    const { width, height, pageSize, plusName, searchVisible } = this.props
     return (
       <div>
-        <Input
+        {searchVisible && (<Input
           style={{ width }}
           onChange={this.handleInputSearch}
           ref={this.searchInputRef}
           type="text"
           size="small"
           placeholder='输入关键词搜索'
-        />
+        />)}
         <div className={css.box} style={{width, height}}>
           {tags.map((tag, index) => {
             const isLongTag = tag.length > 20;
@@ -130,7 +188,10 @@ export default class Tasks extends Component {
                   type: 'close',
                   event: this.handleClose
                 }}
+                index={index}
+                val={tag}
                 onClick = {this.handleSelected}
+                dataUpdate={this.dataUpdate}
               >
                 {isLongTag ? `${tag.slice(0, 20)}...` : tag}
               </MyTag>
